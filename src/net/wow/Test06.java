e0279462ce12b95e93ad7bd24aa334d46c6cb0c8
Naruephol koron <nrpkr1903@gmail.com>
2026-01-13 16:15:17 +0700

    สร้างโปรเจ็กต์ และเขียนโค้ด Test05 - Test06

--- src/net/wow/Test06.java
+++ src/net/wow/Test06.java
@@ -1,4 +1,43 @@
 package net.wow;
 
+//รับชื่อ แล้วเลือกแบบเมนู A, Hello, ..... B, Hi, .... C, Hey, ....
+
+import java.util.Scanner;
+
 public class Test06 {
+    public  static void main(String[] args) {
+        String fullname;
+        int menu;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("ป้อนชื่อ :");
+        fullname = scanner.next();
+        System.out.println("----------------------------------");
+        System.out.println("             เมนูทักทาย");
+        System.out.println("----------------------------------");
+        System.out.println("1) Hello");
+        System.out.println("2) Hi");
+        System.out.println("3) Hey");
+        System.out.println("----------------------------------");
+        System.out.println("เลือกเมนู: ");
+        menu = scanner.nextInt();
+        System.out.println("----------------------------------");
+
+        switch ( menu ){
+            case 1 :
+                System.out.println("Hello," + fullname);
+                break;
+            case 2 :
+                System.out.println("Hi," + fullname);
+                break;
+            case 3 :
+                System.out.println("Hey," + fullname);
+                break;
+            default :
+                System.out.println("เลือกเมนูผิด...!");
+        }
+
+        System.out.println("----------------------------------");
+    }
 }
--- src/th/ac/sau/Test05.java
+++ src/th/ac/sau/Test05.java
@@ -1,4 +1,22 @@
 package th.ac.sau;
 
+import java.util.Scanner;
+
+//รับคะแนนเลขจำนวณเต็มทางแป้นพิมพ แล้วแสดงผล 50 ชิ้นผ่านไป ไม่ถึงไม่ผ่าน
+
 public class Test05 {
+    public static void main(String[] args) {
+        int score;
+        String result;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("ป้อนคะแนน: ");
+        score = scanner.nextInt();
+        System.out.println("---------------------------");
+        result = score >= 50 ? "ผ่าน" : "ไม่ผ่าน";
+
+        System.out.println("ผลคะแนนคือ: " + result);
+
+    }
 }
